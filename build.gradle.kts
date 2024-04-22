43eaefaf2dba1fde15050526274d508747171c58
Kostas Giapis <kgstradviolin@outlook.com>
2024-04-22 21:54:32 +0300

    deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,8 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.3.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.android.tools.build:gradle:8.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
