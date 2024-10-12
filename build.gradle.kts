a4b227ac0f99bbfcd55155a691883a20794bdaeb
Kostas Giapis <kgstradviolin@outlook.com>
2024-10-12 11:53:03 +0300

    deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ android {
     defaultConfig {
         applicationId = "com.machiav3lli.backup"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 8320
         versionName = "8.3.10"
         buildConfigField("int", "MAJOR", "8")
